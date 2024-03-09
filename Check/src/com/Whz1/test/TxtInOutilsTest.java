cfb1a264ce67f30a879b412739dbc208dc376756
jiupan1 <1986951754@qq.com>
2024-03-10 00:50:01 +0800

    first commit

---
+++ Check/src/com/Whz1/test/TxtInOutilsTest.java
@@ -0,0 +1,40 @@
+import com.Whz1.utils.TxtInOutils;
+
+public class TxtInOutilsTest {
+
+
+
+    @org.junit.Test
+    public void readTxt() {
+        // 路径存在，正常读取
+
+        String str = TxtInOutils.readTxt("D:/test/orig.txt");
+        String[] strings = str.split(" ");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    @org.junit.Test
+    public void writeTxt() {
+        // 路径存在，正常写入
+        double[] elem = {0.11, 0.22, 0.33, 0.44, 0.55};
+        for (int i = 0; i < elem.length; i++) {
+            TxtInOutils.writeTxt(elem[i], "D:/test/ans.txt");
+        }
+    }
+    @org.junit.Test
+    public void readTxtFailTest() {
+        // 路径不存在，读取失败
+        String str = TxtInOutils.readTxt("D:/test/none.txt");
+    }
+    @org.junit.Test
+    public void writeTxtFailTest() {
+        // 路径错误，写入失败
+        double[] elem = {0.11, 0.22, 0.33, 0.44, 0.55};
+        for (int i = 0; i < elem.length; i++) {
+            TxtInOutils.writeTxt(elem[i], "User:/test/ans.txt");
+        }
+    }
+
+}
