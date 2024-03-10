b59fa9339b0cab541e62cf3ae6d8948d8bca6339
jiupan1 <1986951754@qq.com>
2024-03-10 17:55:48 +0800

    second commit

---
+++ PaperCheck/src/test/java/SimHashUtilsTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import utils.SimHashUtils;
+import utils.TxtInOutils;
+
+public class SimHashUtilsTest {
+
+    @Test
+    public void getHashTest() {
+        String[] strings = {"原神", "是", "一款", "好玩", "的", "游戏"};
+        for (String string : strings) {
+            String stringHash = SimHashUtils.getHash(string);
+            System.out.println(stringHash.length());
+            System.out.println(stringHash);
+        }
+    }
+
+    @Test
+    public void getSimHashTest() {
+        String str0 = TxtInOutils.readTxt("D:/test/orig.txt");
+        String str1 = TxtInOutils.readTxt("D:/test/orig_0.8_add.txt");
+        System.out.println(SimHashUtils.getSimHash(str0));
+        System.out.println(SimHashUtils.getSimHash(str1));
+    }
+}
